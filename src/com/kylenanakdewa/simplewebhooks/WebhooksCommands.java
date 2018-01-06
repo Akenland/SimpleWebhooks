3240a1614f3c3f23d86131442d8af8148aaf84ef
Kyle Nanakdewa <KyleN@live.ca>
2018-01-05 20:09:24 -0500

    add useragent to bypass 403

--- src/com/kylenanakdewa/simplewebhooks/WebhooksCommands.java
+++ src/com/kylenanakdewa/simplewebhooks/WebhooksCommands.java
@@ -100,6 +100,7 @@ public class WebhooksCommands implements TabExecutor {
                 URLConnection connection = url.openConnection();
                 connection.setRequestProperty("Accept-Charset", java.nio.charset.StandardCharsets.UTF_8.name());
                 connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 
                 if(args.length==3){
                     connection.setDoOutput(true);
