4d732e539cc5b1a1a92fcc88349e9e50f0bed03b
Kyle Nanakdewa <KyleN@live.ca>
2018-03-21 21:42:51 -0400

    added sender coloring, improved formatting for Discord sync

--- src/com/kylenanakdewa/chateverywhere/DiscordListener.java
+++ src/com/kylenanakdewa/chateverywhere/DiscordListener.java
@@ -1,6 +1,9 @@
 package com.kylenanakdewa.chateverywhere;
 
+import java.awt.Color;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.dynmap.DynmapAPI;
 
 import sx.blah.discord.api.ClientBuilder;
@@ -24,13 +27,60 @@ public final class DiscordListener {
     public void onChat(MessageReceivedEvent event){
         // Ignore if it's a webhook message
         if(event.getMessage().getWebhookLongID()==0){
-            String message = "<"+event.getAuthor().getDisplayName(event.getGuild())+">" + event.getMessage().getFormattedContent();
+            ChatColor realmColor = colorToChatColor(event.getAuthor().getColorForGuild(event.getGuild()));
+            String sender = event.getAuthor().getDisplayName(event.getGuild());
+            String message = event.getMessage().getFormattedContent();
 
             // Send it to the server
-            Bukkit.broadcastMessage(message);
+            Bukkit.broadcastMessage(realmColor+"<"+sender+"> "+ChatColor.RESET + message);
 
             // Send it to dynmap
-            ((DynmapAPI)Bukkit.getPluginManager().getPlugin("dynmap")).sendBroadcastToWeb(null, message);
+            ((DynmapAPI)Bukkit.getPluginManager().getPlugin("dynmap")).sendBroadcastToWeb(realmColor+sender, message);
         }
+    }
+
+    /**
+     * Converts a {@link Color} to a Bukkit ChatColor.
+     * The color must match the Minecraft colors exactly. If an exact match is not found, this method will return null.
+     * @param color the color to convert
+     * @return the matching ChatColor, or null if an exact match does not exist
+     */
+    private static ChatColor colorToChatColor(Color color){
+        if(color.getRed()==0 && color.getGreen()==0 && color.getBlue()==0)
+            return ChatColor.BLACK;
+        if(color.getRed()==0 && color.getGreen()==0 && color.getBlue()==170)
+            return ChatColor.DARK_BLUE;
+        if(color.getRed()==0 && color.getGreen()==170 && color.getBlue()==0)
+            return ChatColor.DARK_GREEN;
+        if(color.getRed()==0 && color.getGreen()==170 && color.getBlue()==170)
+            return ChatColor.DARK_AQUA;
+        if(color.getRed()==170 && color.getGreen()==0 && color.getBlue()==0)
+            return ChatColor.DARK_RED;
+        if(color.getRed()==170 && color.getGreen()==0 && color.getBlue()==170)
+            return ChatColor.DARK_PURPLE;
+        if(color.getRed()==255 && color.getGreen()==170 && color.getBlue()==0)
+            return ChatColor.GOLD;
+        if(color.getRed()==170 && color.getGreen()==170 && color.getBlue()==170)
+            return ChatColor.GRAY;
+        if(color.getRed()==85 && color.getGreen()==85 && color.getBlue()==85)
+            return ChatColor.DARK_GRAY;
+        if(color.getRed()==85 && color.getGreen()==85 && color.getBlue()==255)
+            return ChatColor.BLUE;
+
+        if(color.getRed()==85 && color.getGreen()==255 && color.getBlue()==85)
+            return ChatColor.GREEN;
+        if(color.getRed()==85 && color.getGreen()==255 && color.getBlue()==255)
+            return ChatColor.AQUA;
+        if(color.getRed()==255 && color.getGreen()==85 && color.getBlue()==85)
+            return ChatColor.RED;
+        if(color.getRed()==255 && color.getGreen()==85 && color.getBlue()==255)
+            return ChatColor.LIGHT_PURPLE;
+        if(color.getRed()==255 && color.getGreen()==255 && color.getBlue()==85)
+            return ChatColor.YELLOW;
+        if(color.getRed()==255 && color.getGreen()==255 && color.getBlue()==255)
+            return ChatColor.WHITE;
+
+        // No match, return null
+        return null;
     }
 }
